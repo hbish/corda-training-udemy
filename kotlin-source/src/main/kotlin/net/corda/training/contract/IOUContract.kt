b0c8db72fb6655a5ed562d9cf271a7cf8b6b2c47
Ben Shi <ben@hbish.com>
2019-11-02 09:17:41 +1100

    training stuff

---
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -0,0 +1,46 @@
+package net.corda.training.contract
+
+import net.corda.core.contracts.*
+import net.corda.core.transactions.LedgerTransaction
+import net.corda.training.state.IOUState
+
+/**
+ * This is where you'll add the contract code which defines how the [IOUState] behaves. Look at the unit tests in
+ * [IOUContractTests] for instructions on how to complete the [IOUContract] class.
+ */
+class IOUContract : Contract {
+    companion object {
+        @JvmStatic
+        val IOU_CONTRACT_ID = "net.corda.training.contract.IOUContract"
+    }
+
+    /**
+     * Add any commands required for this contract as classes within this interface.
+     * It is useful to encapsulate your commands inside an interface, so you can use the [requireSingleCommand]
+     * function to check for a number of commands which implement this interface.
+     */
+    interface Commands : CommandData {
+        // Add commands here.
+        // E.g
+        // class DoSomething : TypeOnlyCommandData(), Commands
+        class Issue : TypeOnlyCommandData(), CommandData
+    }
+
+    /**
+     * The contract code for the [IOUContract].
+     * The constraints are self documenting so don't require any additional explanation.
+     */
+    override fun verify(tx: LedgerTransaction) {
+        val command = tx.commands.requireSingleCommand<Commands.Issue>()
+
+        requireThat {
+            "No inputs should be consumed when issuing an IOU." using tx.inputs.isEmpty()
+            "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
+            val outputState = tx.outputStates.single() as IOUState
+            "A newly issued IOU must have a positive amount." using (outputState.amount > Amount(0, outputState.amount.token))
+            "The lender and borrower cannot have the same identity." using (outputState.lender != outputState.borrower)
+            "Both lender and borrower together only may sign IOU issue transaction." using
+                    (command.signers.toSet() == outputState.participants.map { it.owningKey }.toSet())
+        }
+    }
+}
